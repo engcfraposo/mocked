f9261c139dfc8b56bfcb052bd4ed52b3884a175d
claudio.f.raposo <claudio.f.raposo@accenture.com>
2021-06-27 15:22:19 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('api.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.db = router.db
+
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
